9503c1a84be3780c44e7db87d5b799142fc378ef
Robson Pedroso <rbndp@hotmail.com>
2020-04-03 17:32:54 -0300

    Initial project

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+Vagrant.configure("2") do |config|
+  config.vm.define "ubuntu" do |ubuntu|
+    # configuration ubuntu image with ubuntu 19
+    ubuntu.vm.box = "ubuntu/eoan64"
+
+    # configuration timeout for 20 minutes
+    ubuntu.vm.boot_timeout = 1200
+
+    # configuration IP for visualization in host
+    ubuntu.vm.network "private_network", ip: "192.168.101.10"
+
+
+    ubuntu.vm.synced_folder ".", "/curso"
+    ubuntu.vm.synced_folder ".", "/vagrant", disabled: true
+
+    ubuntu.vm.provision "shell", inline: <<-SHELL
+      apt-get update
+      apt-get install -y docker.io
+      apt-get install -y docker-compose
+      SHELL
+  end
+end
---
+++ start_all.sh
@@ -0,0 +1,16 @@
+JOIN_RABBIT2_RABBIT1="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
+JOIN_RABBIT3_RABBIT1="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
+JOIN_RABBIT4_RABBIT1="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
+
+echo -n "Starting container..."
+docker-compose down
+
+rm -rf storage
+docker-compose up -d
+
+sleep 15
+
+docker exec -ti rabbitmq2 bash -c "$JOIN_RABBIT2_RABBIT1"
+docker exec -ti rabbitmq3 bash -c "$JOIN_RABBIT3_RABBIT1"
+docker exec -ti rabbitmq4 bash -c "$JOIN_RABBIT4_RABBIT1"
+docker exec -ti rabbitmq1 bash -c
