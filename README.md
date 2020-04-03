50cb7f3480bed0edd704a6b16d5058e829d0edc9
Robson Pedroso <rbndp@hotmail.com>
2020-04-03 17:50:09 -0300

    readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# Lab - RabbitMQ
+
+Projeto para testar o RabbitMQ com cluster.
+Será gerado um master e 3 nós para o cluster.
+
+## Introdução
+
+Essas instruções fornecerão uma cópia do projeto em execução na sua máquina local para fins de desenvolvimento e teste.
+
+### Prerequisitos
+
+O que você precisa para baixar, rodar e disponibilizar.
+
+* Vagrant
+* VirtualBox
+
+### Instalação
+
+Após a execução do pre requisitos, segue um passo a passo de como rodar localmente.
+
+Clonar o repositório
+
+```
+git clone git@github.com:robsonpedroso/lab-rabbitmq.git
+```
+
+Rodar o comando `vagrant up` para subir o server com o RabbitMQ instalado
+ - IP do servidor `192.168.101.10`
+	- Porta do dashboard: 8080
+ - Usuário: `admin`
+ - Senha: `123456`
+
+## Publicação
+
+Não  foi publicado
+
+## Autores
+
+* **Robson Pedroso** - *Projeto inicial* - [RobsonPedroso](https://github.com/robsonpedroso)
+
+## License
+
+Software feito apenas para fins de estudo
+
+## Ferramentas
+
+* [RabbitMQ](https://www.rabbitmq.com/getstarted.html)
+* [Vagrant](https://www.vagrantup.com/)
+* [VirtualBox](https://www.virtualbox.org/)
--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,6 @@
-JOIN_RABBIT2_RABBIT1="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
-JOIN_RABBIT3_RABBIT1="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
-JOIN_RABBIT4_RABBIT1="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
+JOIN_RABBIT2="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
+JOIN_RABBIT3="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
+JOIN_RABBIT4="rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app"
 
 echo -n "Starting container..."
 docker-compose down
@@ -10,7 +10,7 @@ docker-compose up -d
 
 sleep 15
 
-docker exec -ti rabbitmq2 bash -c "$JOIN_RABBIT2_RABBIT1"
-docker exec -ti rabbitmq3 bash -c "$JOIN_RABBIT3_RABBIT1"
-docker exec -ti rabbitmq4 bash -c "$JOIN_RABBIT4_RABBIT1"
+docker exec -ti rabbitmq2 bash -c "$JOIN_RABBIT2"
+docker exec -ti rabbitmq3 bash -c "$JOIN_RABBIT3"
+docker exec -ti rabbitmq4 bash -c "$JOIN_RABBIT4"
 docker exec -ti rabbitmq1 bash -c
